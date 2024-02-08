4e86adb7f0c9eeee162495e00d429ed4f975f788
Leo Keselman <67652134+lkeselman@users.noreply.github.com>
2024-02-08 15:53:46 -0800

    Feature/remove conda (#8)
    * Initial structure, initial generic topic sub setup
    
    * Initial ingestion, formatting and output to json working (except for arrays like images)
    
    * Local json export and images working, adding conda dep file
    
    * Ignoring commander for now
    
    * Utils feature update
    
    * Add conda (#1)
    
    * Add conda earthfile + devcontainer
    
    * Add collector
    
    * Conda gymnastics
    
    * Building conda
    
    * Save source and tinyxml
    
    * Dev env logic reversal
    
    * Added outputters interface and s3 def structure
    
    * Upload to s3 working
    
    * Working format and upload according to existing standard
    
    * Added proper configuration of pipelines, credentials actually work without env vars
    
    * Added rosbag formatter, fs outputter. rosbag formatter needs some work
    
    * Efficiency improvements, rosbag working
    
    * Update working directory for CI builds
    
    * Chart updates
    
    * Template update
    
    * Chart name update
    
    * Chart update to reflect correct image name in registry
    
    * Launch args and default node name
    
    * Volume mount troubles
    
    * updating library path before launch
    
    * Added ld_library path to entrypoint
    
    * Messed up launch args
    
    * Sysmon env vars added
    
    * Fixing copy paste error in deployment
    
    * Added sysmon health checks and affinity
    
    * Added commander and bindings, allow setting of snapshot name from commander
    
    * Added ros param output of existing data collectors for use with debug gui
    
    * Ros param illegal character fix
    
    * Rosparam more issues
    
    * Added service to get time to be used by debug gui
    
    * Updated filesystem mounting and beginning of tf buffer data storage
    
    * Added wasabi cred file env var
    
    * Updated AWS credentials to use default provider chain
    
    * No cache on ROS build
    
    * Putting back to cached
    
    * Updated env var name
    
    * Added notes in readme about configuration, design, etc.)
    
    * Starting PR fixes
    
    * Some updates from PR comments
    
    * Updating const to constexpr
    
    * Constexpr need to be static
    
    * Reverting back to normal deps for consistency
    
    * Pulling aws from http
    
    * Extraneous cd
    
    * S3 Client constructor for new api + install only s3 and transfer
    
    * Copying source better
    
    * Putting AWS SDK level up to make dev container opening easier
    
    * Merge errors
    
    * Removed entrypoint - no longer used
    
    ---------
    
    Co-authored-by: carlosatrios <101591690+carlosatrios@users.noreply.github.com>
    Co-authored-by: Carlos Fernandez-Martinez <carlos.fernandez@rios.ai>

--- docker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# setup  environment
-source "/opt/ros/$ROS_DISTRO/setup.bash"
-source "/opt/rios/install/setup.bash"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rios/install/lib
-
-exec "$@"
